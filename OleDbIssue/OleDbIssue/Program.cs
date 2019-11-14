0908f15720b3f20af5afe00679162692f7a3b412
Vladimir Drobyshev <vladimir.drobyshev@devexpress.com>
2019-11-14 17:48:03 +0300

    Upload

---
+++ OleDbIssue/OleDbIssue/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace OleDbIssue {
+    class Program {
+        static void Main(string[] args) {
+            SalesGenerator generator = new SalesGenerator();
+            string connectionString = string.Format("Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0}\\sales.mdb", Environment.CurrentDirectory);
+            generator.Run(connectionString);
+        }
+    }
+}
